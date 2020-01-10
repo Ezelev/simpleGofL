46b4678232fb6401e5aa1d2a2fbf1ef8dd8f96e7
ezelev <iiezelev@gmail.com>
2020-01-10 19:21:28 +0300

    some refactor and add basic console version

--- game.php
+++ game.php
@@ -1,53 +1,47 @@
-<!DOCTYPE html>
-<html>
-<head>
-<link rel="stylesheet" type="text/css" href="/css/styles.css">
-<script src="/js/script.js"></script>
-</head>
-<title></title>
-<body>
-	<table border="1">
-		<tr>
-			<td id="00">0</td>
-			<td id="01">0</td>
-			<td id="02">0</td>
-			<td id="03">0</td>
-			<td id="04">0</td>
-		</tr>
-		<tr>
-			<td id="10">0</td>
-			<td id="11">0</td>
-			<td id="12">0</td>
-			<td id="13">0</td>
-			<td id="14">0</td>
-		</tr>
-		<tr>
-			<td id="20">0</td>
-			<td id="21" class="active">1</td>
-			<td id="22" class="active">1</td>
-			<td id="23" class="active">1</td>
-			<td id="24">0</td>
-		</tr>
-		<tr>
-			<td id="30">0</td>
-			<td id="31">0</td>
-			<td id="32">0</td>
-			<td id="33">0</td>
-			<td id="34">0</td>
-		</tr>
-		<tr>
-			<td id="40">0</td>
-			<td id="41">0</td>
-			<td id="42">0</td>
-			<td id="43">0</td>
-			<td id="44">0</td>
-		</tr>
-	</table>
+<?php
+require_once("gameoflife.php");
+$n = 10;
+$m = 10;
+$pattern = "blinker";
+$cyclesCount = 10;
 
-	<div id="test">
+if($n && $m && $pattern){
+  $gol = new GameOfLife(true, $n, $m, $pattern);
+//
+  $start = microtime(true);
+//
+  $gol->gameOfLife($cyclesCount);
+//
+  ob_clean();
 
-	</div>
+  $responseBody = $gol->getEvolutionHistory();
+  // $time_elapsed_secs = microtime(true) - $start;
+  // $response = ["status" => "1", "message" => "success", "evolution_exec_time"=> $time_elapsed_secs,"body" => $responseBody];
+	//
+ // echo json_encode($response);
+ ob_start();
+ while(TRUE){
+	 foreach($responseBody as $step){
+	  ob_clean();
+	 	for($i=0;$i<10;$i++){
 
-	<button id="evolve-btn" class="evolve-btn">Start</button>
-</body>
-</html>
+	 		for($j=0;$j<10;$j++){
+	 				echo $step[$i][$j];
+
+	 		}
+	 		echo "\n";
+	 	}
+	 	usleep(500);
+	 }
+	 break;
+ }
+
+  die();
+} else {
+  $response = ["status"=> "0", "message"=> "Wrong parameters."];
+  echo json_encode($response);
+  die();
+}
+
+
+?>
--- index.php
+++ index.php
@@ -7,65 +7,54 @@
   </head>
   <title></title>
 <body>
-<div class="w3-container w3-teal">
-  <h1>GofL</h1>
-</div>
-<div class="w3-row">
-      <div class="w3-col m4 l3">
-    <div class="w3-col m12">
-      <div class="w3-container fieldwrap  w3-border w3-center">
-            <p>Select field size:</p>
-            <p>
-              <label for="size-n">N:</label>
-              <input id="size-n" class="w3-input w3-border" type="number" name="size-n" value=5>
-            </p>
-            <p>
-              <label for="size-n">M:</label>
-              <input id="size-m" class="w3-input w3-border" type="number" name="size-n" value=5>
-            </p>
+  <div class="w3-padding-64 w3-margin-bottom w3-center w3-teal">
+    <h1 class="w3-jumbo">Game of Life</h1>
+  </div>
+
+  <div class="w3-row-padding w3-content" style="max-width:1400px">
+    <div class="w3-twothird">
+
+      <h2><a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">Conway's Game of Life</a></h2>
+      <div class="w3-justify">
+        <p>
+          The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970.
+        </p>
+        <p>  The game is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial configuration and observing how it evolves.
+        </p>
+        <img src="images/Gospers_glider_gun.gif" alt="glider-gun" style="width:50%">
+        <p>
+          The universe of the Game of Life is an infinite, two-dimensional orthogonal grid of square cells, each of which is in one of two possible states, alive or dead, (or populated and unpopulated, respectively). Every cell interacts with its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent. At each step in time, the following transitions occur:
+        </p>
+        <ol>
+          <li>Any live cell with fewer than two live neighbours dies, as if by underpopulation.</li>
+          <li>Any live cell with two or three live neighbours lives on to the next generation.</li>
+          <li>Any live cell with more than three live neighbours dies, as if by overpopulation.</li>
+          <li>Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</li>
+        </ol>
+<p>These rules, which compare the behavior of the automaton to real life, can be condensed into the following:</p>
+<ol>
+  <li>Any live cell with two or three neighbors survives.</li>
+  <li>Any dead cell with three live neighbors becomes a live cell.</li>
+  <li>All other live cells die in the next generation. Similarly, all other dead cells stay dead.</li>
+</ol>
+
+<p>The initial pattern constitutes the seed of the system. The first generation is created by applying the above rules simultaneously to every cell in the seed; births and deaths occur simultaneously, and the discrete moment at which this happens is sometimes called a tick. Each generation is a pure function of the preceding one. The rules continue to be applied repeatedly to create further generations.
+</p>
+        </p>
       </div>
+
     </div>
-    <div class="w3-col m12">
-      <div class="w3-container fieldwrap  w3-border w3-center">
-            <p>Select starting pattern:</p>
-            <p>
-                <select id="pattern-select" class="" name="">
-                    <option value="blinker">Blinker</option>
-                    <option value="toad">Toad</option>
-                    <option value="beacon">Beacon</option>
-                    <option value="glider">Glider</option>
-                    <option value="acorn">Acorn</option>
-                </select>
-            </p>
+    <div class="w3-third">
+      <div class="w3-container w3-light-grey">
+        <h2>First version ready!</h2>
+        <p class="w3-justify">
+          First version of my Game of Life online implementation is ready!
+          <a href="gofl.php">Try Game of life -></a>
+        </p>
       </div>
-    </div>
-    <div class="w3-col m12">
-      <div class="w3-container fieldwrap  w3-border w3-center">
-            <p>Enter number of cycles:</p>
-            <p>
-              <input id="cycles-count" class="w3-input w3-border" type="number" name="cycles-count" value=10>
-            </p>
-      </div>
-    </div>
-    <div class="w3-col m12">
-      <div class="w3-container fieldwrap  w3-border w3-center">
-            <button id="evolve-btn" class="w3-button w3-teal evolve-btn">Start</button>
-        <p><i>All steps computed on server side at once. Then render.</i></p>
-      </div>
-    </div>
-    <div class="w3-col m12">
-      <div class="w3-container fieldwrap  w3-border w3-center">
-            <button id="evolve-btn-ajax" class="w3-button w3-teal evolve-btn">Start (test)</button>
-            <p><i>Feature in progress. Launches evolution in ajax mode. Steps are executing and rendering one by one.</i></p>
-    </div>
     </div>
   </div>
-  <div class="w3-col m8 l9 w3-border table-wrap" style="padding-top: 20px;padding-bottom: 20px;text-align: center;">
-    <table id="table-field" border="1" style="">
-    </table>
-  </div>
-</div>
-	<div class="fieldwrap">
-	</div>
+
+  </body>
 </body>
 </html>
